735f9918941c4f23eb969b1e3d5550bd6ebc4d78
Youngseo Yoon <58525719+yys88699@users.noreply.github.com>
2022-05-01 17:35:30 +0900

    Xcode Proj 추가

---
+++ ZeroGame/ZeroGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZeroGame
+//
+//  Created by Youngseo Yoon on 2022/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZeroGame/ZeroGame/ZeroGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroGameApp.swift
+//  ZeroGame
+//
+//  Created by Youngseo Yoon on 2022/05/01.
+//
+
+import SwiftUI
+
+@main
+struct ZeroGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
